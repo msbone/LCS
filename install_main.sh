d20001a30a034f7f59c753bb9736abfbc590f3f3
Ole Mathias Aa. Heggem <msbone1995@gmail.com>
2014-12-30 21:02:04 +0100

    some bug fixes

--- install_main.sh
+++ install_main.sh
@@ -7,9 +7,9 @@ cd "/lcs"
 echo "Downloading code....."
 wget https://github.com/msbone/lcs/archive/master.zip
 unzip -u master.zip;
-mv lcs-master/* ./
+mv LCS-master/* ./
 rm master.zip
-rm -R lcs-master
+rm -R LCS-master
 cp config.pm.example config.pm
 
-echo "lcs is finished instaling (/lcs/), Fill in the missing info in config.pm then run setup_database.sh"
+echo "LCS is finished instaling (/lcs/), Fill in the missing info in config.pm then run setup_database.sh"
