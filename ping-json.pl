7d216a74fd96a25085870df4c7716566c1486d98
Ole Mathias Aa. Heggem <msbone1995@gmail.com>
2015-01-03 11:44:21 +0100

    Nyttårslan, added start of ping systen

---
+++ ping-json.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use DBI;
+use JSON;
+
+require "config.pm";
+
+my %json = ();
+
+# Connect to the database.
+$dbh = DBI->connect("dbi:mysql:$lcs::config::db_name",$lcs::config::db_username,$lcs::config::db_password) or die "Connection Error: $DBI::errstr\n";
+$sql = "select * from switches WHERE ip IS NOT NULL";
+$sth = $dbh->prepare($sql);
+
+$sth->execute or die "SQL Error: $DBI::errstr\n";
+
+while (my $ref = $sth->fetchrow_hashref()) {
+  $json{$ref->{'name'}}{'status'} = $ref->{'ping'};
+}
+my $output = encode_json \%json;
+print $output;
